0b7b3fb74816af4aa20363cbb9a568649a560969
HAMM3R67 <tcschauer@gmail.com>
2015-08-15 13:44:04 -0600

    working on the pending directive function still

--- mainCtrl.js
+++ mainCtrl.js
@@ -3,10 +3,14 @@ var app = angular.module('directiveWorkshop', ['myDirectives']);
 app.controller('mainCtrl', function($scope, mainService){
 
 $scope.getData = function () {
-        return mainService.getData($scope.query).then(function (data) {
+        return mainService.getData($scope.query).then().then(function (data) {
            console.log(data);
            return $scope.data = data;
-        });
+        });  
     };
+    
+   $scope.pending = pending
+    
+    
 
 });
--- myDirectives.js
+++ myDirectives.js
@@ -1,11 +1,20 @@
 var app = angular.module('myDirectives', []);
 
-app.directive('pending', function(){
+app.directive('pending', function($q){
 	return {
 		restrict: 'AE',
 		scope: '&',
 		link: function(scope, elem, attrs){
 			console.log(scope, elem, attrs)
-		}
+			var deferred = $q.defer();
+			
+			var toggle = function (){
+			deferred.resolve(scope.isVisible === true)
+    		scope.toggle = function(){
+        	scope.isVisible = !scope.isVisible;	
+			}
+			
+    	}
+	  }
 	}
-})
+});
